75dd7194bbbe355b759f0596b36397e765609b42
joni2back <joni2back@gmail.com>
2013-12-24 08:13:55 -0300

    effects

--- src/cool.js
+++ src/cool.js
@@ -4,36 +4,40 @@
 
 Math.fromto = function (from, to) {
     return Math.floor(Math.random() * to) + from;
-}
+};
 
-function CoolGame() {
+Image.create =  function(src) {
+    var img = new Image();
+    img.src = src;
+    return img;
+};
+
+var CoolGame = function() {
     this.fps = 60;
     this.canvas = null;
     this.width = 0;
     this.height = 0;
     this.minVelocity = 200;
     this.maxVelocity = 3000;
-    this.stars = 50;
+    this.balls = 25;
     this.intervalId = 0;
-    this.clickedStars = 0;
+    this.clickedBalls = 0;
     this.audio = 'click.wav';
     this.imgBall = 'ball.png';
-    this.imgBallOver = 'ball_over.png'
-}
+    this.imgBallOver = 'ball_over.png';
+};
 
-function Star(x, y, width, height, velocity) {
+var Ball = function(image, x, y, width, height, velocity) {
+    this.image = Image.create(image);
     this.x = x;
     this.y = y || -49;
     this.width = width || 47;
     this.height = height || 49;
     this.velocity = velocity || Math.fromto(100,400);
-    this.image = new Image();
-    this.image.src = 'ball.png';
     this.alive = true;
-}
+};
 
-//The main function - initialises the coolGame.
-CoolGame.prototype.initialise = function (div) {
+CoolGame.prototype.init = function (div) {
     var self = this;
 
     //Store the div.
@@ -57,98 +61,117 @@ CoolGame.prototype.initialise = function (div) {
     this.canvas.height = this.height;
 };
 
-CoolGame.prototype.start = function () {
-
-    //Create the stars.
-    var stars = [];
-    for (var i = 0; i < this.stars; i++) {
-        stars[i] = new Star(Math.random() * this.width, Math.random() * this.height);
+CoolGame.prototype.ballt = function () {
+    //Create the balls.
+    var balls = [];
+    for (var i = 0; i < this.balls; i++) {
+        balls[i] = new Ball(
+            this.imgBall,
+            Math.random() * this.width,
+            Math.random() * this.height
+        );
     }
-    this.stars = stars;
+    this.balls = balls;
+    this.resume();
+};
 
+CoolGame.prototype.pause = function () {
+    clearInterval(this.intervalId);
+};
+
+CoolGame.prototype.resume = function () {
     var self = this;
-    //Start the timer.
     this.intervalId = setInterval(function () {
         self.update();
         self.draw();
     }, 1000 / this.fps);
 };
 
-CoolGame.prototype.stop = function () {
-    clearInterval(this.intervalId);
-};
-
 CoolGame.prototype.update = function () {
     var dt = 1 / this.fps;
 
-    for (var i = 0; i < this.stars.length; i++) {
-        var star = this.stars[i];
-        star.y += dt * star.velocity;
-        //If the star has moved from the bottom of the screen, spawn it at the top.
-        if (star.y > this.height) {
-            this.stars[i] = new Star(Math.random() * this.width);
+    //Walk around the previously created balls
+    for (var i = 0; i < this.balls.length; i++) {
+        var ball = this.balls[i];
+        ball.y += dt * ball.velocity;
+        //If the ball has moved from the bottom of the screen, spawn it at the top.
+        if (ball.y > this.height) {
+            this.balls[i] = new Ball(
+                this.imgBall,
+                Math.random() * this.width
+            );
         }
     }
 };
 
 CoolGame.prototype.draw = function () {
-
-    //Get the drawing context.
     var ctx = this.canvas.getContext("2d");
 
     //Draw the background.
     ctx.fillStyle = '#fff';
     ctx.fillRect(0, 0, this.width, this.height);
-    ctx.fillStyle = this.stars[1].color;
+    //ctx.fillStyle = this.balls[1].color;
 
-    for (var i = 0; i < this.stars.length; i++) {
-        var star = this.stars[i];
-        ctx.drawImage(star.image, star.x, star.y, star.width, star.height);
-        //ctx.fillStyle = star.color;
-        //ctx.fillStyle = pattern;
-        //ctx.fillRect(star.x, star.y, star.width, star.height);
-        ctx.moveTo(star.y, star.x);
+    for (var i = 0; i < this.balls.length; i++) {
+        var ball = this.balls[i];
+        this._drawBall(ball, ctx);
     }
 };
 
-CoolGame.prototype.starClick = function (star) {
-    if (star.alive) {
-        star.alive = false;
-        var img = new Image(this.imgBallOver);
-        var snd = new Audio(this.audio);
-        img.src = this.imgBallOver;
+CoolGame.prototype._drawBall = function (ball, ctx) {
+    //boxes
+    //ctx.fillStyle = ball.color;
+    //ctx.fillStyle = pattern;
+    //ctx.fillRect(ball.x, ball.y, ball.width, ball.height);
+    //ctx.moveTo(ball.y, ball.x);
 
-        star.image = img; 
-        snd.play();
-        star.velocity = star.velocity * -1;
-        this.clickedStars++;
+    //circles
+    //ctx.beginPath();
+    //ctx.arc(ball.x, ball.y, ball.width /2, 159, 2 * Math.PI, false);
+    //ctx.fillStyle = 'red';
+    //ctx.fill();
+    //ctx.moveTo(ball.y, ball.x);
+
+    ctx.drawImage(ball.image, ball.x, ball.y, ball.width, ball.height);
+    ctx.moveTo(ball.y, ball.x);
+};
+
+CoolGame.prototype.ballClick = function (ball) {
+    if (ball.alive) {
+        new Audio(this.audio).play();
+        ball.alive = false;
+        ball.image.src = this.imgBallOver;
+        ball.velocity = ball.velocity * -1;
+        this.clickedBalls++;
+
+        //Regenerate lost balls
+        //this.balls.push(new Ball(
+        //    this.imgBall,
+        //    Math.random() * this.width
+        //));
     }
 };
 
-CoolGame.prototype.click = function (x, y, e) {
-    for (var i = 0; i < this.stars.length; i++) {
-        var left = this.stars[i].x;
-        var right = this.stars[i].x + this.stars[i].width;
-        var top = this.stars[i].y;
-        var bottom = this.stars[i].y + this.stars[i].height;
+CoolGame.prototype.click = function (x, y) {
+    for (var i = 0; i < this.balls.length; i++) {
+        var left = this.balls[i].x;
+        var right = this.balls[i].x + this.balls[i].width;
+        var top = this.balls[i].y;
+        var bottom = this.balls[i].y + this.balls[i].height;
         if (right >= x && left <= x && bottom >= y && top <= y) {
-            e.style.cursor = 'pointer';
-            this.starClick(this.stars[i]);
+            this.ballClick(this.balls[i]);
             return true;
-        } else {
-            e.style.cursor = 'default';
         }
     }
 };
 
+window.onload = function() {
+    var coolGame = new CoolGame();
+    var container = document.getElementById('game');
+    container.addEventListener('mousemove', function (e) {
+        coolGame.click(e.offsetX, e.offsetY);
+    }, false);
 
-var coolGame = new CoolGame();
-
-var container = document.getElementById('game');
-container.addEventListener('mousemove', function (e) {
-    coolGame.click(e.offsetX, e.offsetY, container);
-}, false);
-
-
-coolGame.initialise(container);
-coolGame.start();
+    coolGame.init(container);
+    coolGame.ballt();
+};
